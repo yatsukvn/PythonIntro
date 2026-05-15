20a348b4b1416ca0f25b58b37e947ff3c887809b
Vladimir <yatsukvn@gmail.com>
2026-05-15 15:21:59 +0600

    Урок 15. Встроенные функции

---
+++ L15/p3.py
@@ -0,0 +1,18 @@
+# 3. Напишите программу, которая запрашивает у пользователя шесть вещественных чисел.
+#    На экран выводит минимальное и максимальное из них, округленные до двух знаков
+#    после запятой. Выполните задание без использования встроенных функций min и max.
+
+numbers = []
+
+for i in range(1, 7):
+    numbers.append(float(input(f'Введите вещественное число № {i}: ')))
+
+minimum = numbers[0]
+maximum = numbers[0]
+for i in range(0, 6):
+    if numbers[i] < minimum:
+        minimum = numbers[i]
+    elif numbers[i] > maximum:
+        maximum = numbers[i]
+
+print(f'Minimum = {minimum:.2F}, maximum = {maximum:.2F}')
