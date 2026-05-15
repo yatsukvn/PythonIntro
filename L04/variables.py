31f388997d8eaffb99d7e0cfcac29ae340843a83
Vladimir <yatsukvn@gmail.com>
2026-05-15 10:16:25 +0600

    добавил комментарии к урокам 4 и 5

--- L04/variables.py
+++ L04/variables.py
@@ -1,18 +1,29 @@
+# 1. Переменной var_int присвойте значение 10, var_float - значение 8.4, var_str - "No"
+
 var_int = 10
 var_float = 8.4
 var_str = 'No'
 
+# 2. Значение, хранимое в переменной var_int, увеличьте в 3.5 раза. Полученный результат
+#    свяжите с переменной var_big.
 var_big = var_int * 3.5
 print(f'var_big = {var_big}')
+
+# 3. Измените значение, хранимое в переменной var_float, уменьшив его на единицу,
+#    результат свяжите с той же переменной.
 
 var_big = var_float - 1
 print(f'var_big = {var_big}')
 
+# 4. Разделите var_int на var_float, а затем var_big на var_float. Результат данных выражений
+#    не привязывайте ни к каким переменным.
 var_a = var_int / var_float
 var_b = var_big / var_float
 
 print(f'var_a = {var_a}, var_b = {var_b}')
 
+# 5. Измените значение переменной var_str на "NoNoYesYesYes". При формировании нового
+#    значения используйте операции конкатенации ( +) и повторения строки ( *).
 var_str = var_str * 2 + 'Yes' * 3
 
 print(f'var_str = "{var_str}"')
--- L05/user.py
+++ L05/user.py
@@ -1,8 +1,20 @@
+# 1. Напишите программу (файл user.py), которая запрашивала бы у пользователя:
+#    - его имя (например, "What is your name?")
+#    - возраст ("How old are you?")
+#    - место жительства ("Where are you live?")
+#   После этого выводила бы три строки:
+#   "This is имя"
+#   "It is возраст"
+#   "(S)he live in место_жительства"
+#   Вместо имя, возраст, место_жительства должны быть данные, введенные
+#   пользователем. Примечание: можно писать фразы на русском языке, но если вы
+#   планируете стать профессиональным программистом, привыкайте к английскому
+
 name = input('What is your name?')
 age = input('How old are you?')
 location = input('Where are you live?')
 
-print(f'name = "{name}"')
-print(f'age = "{age}"')
-print(f'location = "{location}"')
+print(f'This is имя "{name}"')
+print(f'It is возраст "{age}"')
+print(f'(S)he live in место_жительства "{location}"')
 
